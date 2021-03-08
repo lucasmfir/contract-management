09bdcf68c69ed32e919ee8c5cefc49c7c5ccf59f
Lucas Firmo <lucasmfir@gmail.com>
2021-03-08 00:30:26 -0300

    formatacao

--- lib/contract_management_web/endpoint.ex
+++ lib/contract_management_web/endpoint.ex
@@ -14,8 +14,9 @@ defmodule ContractManagementWeb.Endpoint do
     websocket: true,
     longpoll: false
 
-  socket "/live", Phoenix.LiveView.Socket,
-    websocket: [connect_info: [session: @session_options]]
+  socket "/live",
+         Phoenix.LiveView.Socket,
+         websocket: [connect_info: [session: @session_options]]
 
   # Serve at "/" the static files from "priv/static" directory.
   #
